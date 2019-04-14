bce1a0d82076571cd2abea1e3ccfc5a1d0a47be2
Mingjiong Liao <liao0019@algonquinlive.com>
2019-04-14 11:24:46 -0400

    Add a data model---course class
    with properties to the class:

---
+++ CoursesManagement/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Course
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Credits { get; set; }
+        public int Length { get; set; }
+    }
+}
