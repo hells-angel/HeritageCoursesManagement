0229a3087e607b24ff715d0bbf395ef6db4912de
Mingjiong Liao <liao0019@algonquinlive.com>
2019-04-14 12:00:12 -0400

    the scaffolding tool produces pages for Create, Read, Update, and Delete (CRUD) operations for the movie model
    Files created:
    Pages/Movies: Create, Delete, Details, Edit, and Index.

---
+++ CoursesManagement/Pages/Courses/Details.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CoursesManagement.Models;
+
+namespace CoursesManagement.Pages.Courses
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CoursesManagement.Models.RazorPagesCourseContext _context;
+
+        public DetailsModel(CoursesManagement.Models.RazorPagesCourseContext context)
+        {
+            _context = context;
+        }
+
+        public Course Course { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Course = await _context.Course.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Course == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
