f55402b67fa9e05ea28fa74cad69478bb4124bab
Mingjiong Liao <liao0019@algonquinlive.com>
2019-04-14 13:51:26 -0400

    database Initial migration
    console:
    dotnet ef migrations add InitialCreate
    dotnet ef database update

---
+++ CoursesManagement/Migrations/20190414175016_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoursesManagement.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Course",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Credits = table.Column<double>(nullable: false),
+                    Length = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Course", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Course");
+        }
+    }
+}
