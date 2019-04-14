fada4855ed2c388f8ddaedfdff85fa34ae14b2a5
Mingjiong Liao <liao0019@algonquinlive.com>
2019-04-14 11:34:38 -0400

    Add a database context class
    Add the RazorPagesCourseContext class to the Models

---
+++ CoursesManagement/Models/RazorPagesCourseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursesManagement.Models
+{
+    public class RazorPagesCourseContext : DbContext
+    {
+        public RazorPagesCourseContext(DbContextOptions<RazorPagesCourseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoursesManagement.Models.Course> Course { get; set; }
+    }
+}
