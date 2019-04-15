c4a2ef6f499126a4564eda1654c33c557f3dda3b
Mingjiong Liao <liao0019@algonquinlive.com>
2019-04-15 18:36:32 -0400

    add validation

--- RazorPagesCourse/Models/Course.cs
+++ RazorPagesCourse/Models/Course.cs
@@ -6,9 +6,21 @@ namespace RazorPagesCourse.Models
     public class Course
     {
         public int ID { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
+
+        
         public string Description { get; set; }
+
+
+        [Range(1, 200)]
         public int Length { get; set; }
+
+        [Range(1, 20)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Credit { get; set; }
     }
 }
