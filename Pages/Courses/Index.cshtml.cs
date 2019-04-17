565995d620b17648419cb6f5e376f95f81eb2868
Mingjiong Liao <liao0019@algonquinlive.com>
2019-04-17 11:34:47 -0400

    final submit for demo

---
+++ Pages/Courses/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesCourse.Models;
+
+namespace RazorPagesCourse.Pages.Courses
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesCourse.Models.RazorPagesCourseContext _context;
+
+        public IndexModel(RazorPagesCourse.Models.RazorPagesCourseContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Course> Course { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Course = await _context.Course.ToListAsync();
+        }
+    }
+}
